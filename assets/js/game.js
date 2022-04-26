ab084e6b699dc12756f0ad5b86cb3953fcd2b2da
Dan Boothe <danboothe92@gmail.com>
2022-04-25 19:05:40 -0600

    MVP setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,7 @@
 const playerName = prompt("What is your robots name?");
 let playerHealth = 100;
 let playerAttack = 10;
-
-console.log(playerName, playerHealth, playerAttack);
+let playerMoney = 10
 
 const enemyName = "Thagalfor";
 let enemyHealth = 50;
@@ -11,21 +10,33 @@ let enemyAttack = 12;
 const fight = function fight() {
     window.alert("Welcome to the Arena!");
 
-    enemyHealth = enemyHealth - playerAttack;
+    let promptFight = confirm("Would you like to skip this fight?");
+    if (promptFight = false) {
+        enemyHealth -= playerAttack;
 
-    if (enemyHealth <= 0) {
-        enemyHealth = 0;
-        alert("Fuck " + enemyName + ", you killed the shit out of them!");
-    } else {
-        alert(enemyName + " now has " + enemyHealth + " HP left!");
-    };
+        if (enemyHealth <= 0) {
+            enemyHealth = 0;
+            alert("Fuck " + enemyName + ", you killed the shit out of them!");
+        } else {
+            alert(enemyName + " now has " + enemyHealth + " HP left!");
+        };
 
-    playerHealth = playerHealth - enemyAttack;
-    if (playerHealth <= 0) {
-        playerHealth = 0;
-        alert(playerName + " died, fucking loser...");
+        playerHealth -= enemyAttack;
+        if (playerHealth <= 0) {
+            playerHealth = 0;
+            alert(playerName + " died, fucking loser...");
+        } else {
+            alert(playerName + " still has " + playerHealth + " HP!");
+        }
     } else {
-        alert(playerName + " still has " + playerHealth + " HP!");
+        let skipConfirm = confirm("Are you sure you want to skip this fight?");
+        if (skipConfirm = true) {
+            alert(playerName + " has pussied out and skipped this fight.");
+
+            playerMoney -= 3;
+        } else {
+            fight();
+        }
     }
 };
 
